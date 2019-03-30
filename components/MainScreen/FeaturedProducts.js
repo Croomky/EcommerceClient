f74de4a8dce3a5c05ad93b3f837c2ffb129ab509
unknown <konrad96pawlak@gmail.com>
2019-03-30 13:30:13 +0100

    add hardcoded 'key' prop to FeaturedProduct

--- components/MainScreen/FeaturedProducts.js
+++ components/MainScreen/FeaturedProducts.js
@@ -17,14 +17,17 @@ export default class FeaturedProducts extends React.Component {
     this.setState({
       productsList: [
         <FeaturedProduct
+          key={1}
           imageUrl={'https://images.pexels.com/photos/1549702/pexels-photo-1549702.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'}
           productTitle={'example product'}
         />,
         <FeaturedProduct
+          key={2}
           imageUrl={'https://images.pexels.com/photos/248412/pexels-photo-248412.jpeg?cs=srgb&dl=bottle-breakfast-clean-248412.jpg&fm=jpg'}
           productTitle={'milk'}
         />,
         <FeaturedProduct
+          key={3}
           imageUrl={'https://images.pexels.com/photos/256298/pexels-photo-256298.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'}
           productTitle={'another example'}
         />
