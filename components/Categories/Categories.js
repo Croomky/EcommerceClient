72bb5edeb18babaa27046569117d8a8b66a306b5
unknown <konrad96pawlak@gmail.com>
2019-03-30 17:19:40 +0100

    create categories component

---
+++ components/Categories/Categories.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { View, ScrollView, Text, StyleSheet } from 'react-native';
+
+export default class Categories extends React.Component {
+  constructor(props) {
+    super(props);
+    this.categoriesList = [];
+    this.prepareCategories();
+  }
+
+  fetchCategories() {
+    return [
+      "Clothes",
+      "Electronics",
+      "Toys",
+      "Sports",
+      "Literature",
+    ];
+  }
+
+  prepareCategories() {
+    const categoriesNames = this.fetchCategories();
+
+    categoriesNames.forEach(name => {
+      this.categoriesList.push(
+        <View style={styles.categoryTile}>
+          <Text style={styles.categoryName}>
+            {name}
+          </Text>
+        </View>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <View>
+        <ScrollView>
+          
+        </ScrollView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  categoryTile: {
+    borderBottom: "black",
+  },
+  categoryName: {
+    fontSize: 19,
+  }
+})
