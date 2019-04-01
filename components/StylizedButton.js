1474ec1bd9fda715d9c3dc52163a3e4b86c1ed32
unknown <konrad96pawlak@gmail.com>
2019-04-01 18:39:24 +0200

    add button with custom style

---
+++ components/StylizedButton.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+import ColorsPalette from './ColorsPalette';
+
+export default class StylizedButton extends TouchableOpacity {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <TouchableOpacity
+        style={styles.button}
+        onPress={this.props.onPress}
+      >
+        <Text
+          style={styles.buttonText}
+        >
+          {this.props.title}
+        </Text>
+      </TouchableOpacity>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: ColorsPalette.main,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 18,
+  },
+  buttonText: {
+    color: ColorsPalette.brightText,
+    fontSize: 16,
+    fontWeight: "400"
+  }
+})
