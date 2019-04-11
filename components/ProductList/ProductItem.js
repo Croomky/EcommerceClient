fdfcea33ce92d08c77409968151bd0d64e54368a
unknown <konrad96pawlak@gmail.com>
2019-04-11 19:24:48 +0200

    stylize product list

--- components/ProductList/ProductItem.js
+++ components/ProductList/ProductItem.js
@@ -13,12 +13,19 @@ export default class ProductItem extends React.Component {
           style={styles.thumbnail}
           source={{ uri: 'https://via.placeholder.com/32' }}
         />
-        <Text style={styles.name}>
-          {this.props.name}
-        </Text>
-        <Text style={styles.price}>
-          {this.props.price}
-        </Text>
+        <View style={styles.productContent}>
+          <View style={styles.namePriceRow}>
+            <Text style={styles.name}>
+              {this.props.name}
+            </Text>
+            <Text style={styles.price}>
+              {this.props.price}
+            </Text>
+          </View>
+          <Text style={styles.productDescription}>
+            lorem ipsum dolor sit amet...
+          </Text>
+        </View>
       </View>
     );
   }
@@ -26,12 +33,37 @@ export default class ProductItem extends React.Component {
 
 const styles = StyleSheet.create({
   mainContainer: {
-    width: '100%',
     flex: 1,
     flexDirection: 'row',
+    width: '90%',
+    height: 100,
+    marginVertical: 5
+  },
+  productContent: {
+    flex: 1,
+    justifyContent: 'flex-start'
+  },
+  namePriceRow: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%'
   },
   thumbnail: {
-    width: 32,
-    height: 32,
+    width: 100,
+    height: 100,
+    marginRight: 5
+  },
+  name: {
+    fontSize: 19
+  },
+  price: {
+    fontSize: 19
+  },
+  productDescription: {
+    flex: 2,
+    marginTop: 5,
+    fontSize: 15
   }
 })
--- components/ProductList/ProductList.js
+++ components/ProductList/ProductList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, ScrollView, StyleSheet } from 'react-native';
 
 import ProductItem from './ProductItem';
 
@@ -18,53 +18,98 @@ export default class ProductList extends React.Component {
           price: '59,99 zl',
           description: 'Short description of the second product.'
         },
+      ],
+      preparedProductList: [
+        <ProductItem
+          key={1}
+          name={'Product 1'}
+          price={'39,99 zl'}
+        />,
+        <ProductItem
+          key={2}
+          name={'Product 2'}
+          price={'59,99 zl'}
+        />,
+        <ProductItem
+          key={3}
+          name={'Product 1'}
+          price={'39,99 zl'}
+        />,
+        <ProductItem
+          key={4}
+          name={'Product 2'}
+          price={'59,99 zl'}
+        />,
+        <ProductItem
+          key={5}
+          name={'Product 1'}
+          price={'39,99 zl'}
+        />,
+        <ProductItem
+          key={6}
+          name={'Product 2'}
+          price={'59,99 zl'}
+        />,
       ]
     }
   }
 
-  fetchProducts() {
-    this.setState({
-      productList: [
-        {
-          name: 'Product 1',
-          price: '39,99 zl',
-          description: 'Short description of the product.'
-        },
-        {
-          name: 'Product 2',
-          price: '59,99 zl',
-          description: 'Short description of the second product.'
-        },
-      ]
-    });
-  }
+  // fetchProducts() {
+  //   this.setState({
+  //     productList: [
+  //       {
+  //         name: 'Product 1',
+  //         price: '39,99 zl',
+  //         description: 'Short description of the product.'
+  //       },
+  //       {
+  //         name: 'Product 2',
+  //         price: '59,99 zl',
+  //         description: 'Short description of the second product.'
+  //       },
+  //     ]
+  //   });
+  // }
 
-  createProductItemsFromState() {
-    const productItems = this.state.productList.map((product, index) => (
-      <ProductItem
-        key={index}
-        name={product.name}
-        price={product.price}
-      />
-    ));
+  // createProductItemsFromState() {
+  //   const productItems = this.state.productList.map((product, index) => (
+  //     <ProductItem
+  //       key={index}
+  //       name={product.name}
+  //       price={product.price}
+  //     />
+  //   ));
     
-    console.log(productItems);
+  //   console.log(productItems);
+  //   this.setState({
+  //     preparedProductList: productItems
+  //   });
 
-    return productItems;
-  }
+  //   return productItems;
+  // }
+
+  // componentDidMount() {
+  //   this.createProductItemsFromState();
+  // }
 
   render() {
     return (
-      <View style={styles.mainContainer}>
-        {this.createProductItemsFromState()}
-      </View>
+      <ScrollView style={styles.mainContainer}
+        contentContainerStyle={styles.contentContainerStyle}
+      >
+        {this.state.preparedProductList}
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
   mainContainer: {
-    paddingTop: 10,
-    width: '90%',
+    flex: 2,
+    width: '100%',
+  },
+  contentContainerStyle: {
+    alignItems: 'center',
+    paddingVertical: 5
   }
 })
