dfc98441d0366a1f7d8b285593d39fa7647084f0
unknown <konrad96pawlak@gmail.com>
2019-04-18 15:31:24 +0200

    stylize shopping cart, add example items

--- components/ShoppingCart/ShoppingCart.js
+++ components/ShoppingCart/ShoppingCart.js
@@ -1,47 +1,76 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, ScrollView, StyleSheet } from 'react-native';
+
+import ShoppingCartItem from './ShoppingCartItem';
+import StylizedButton from '../StylizedButton';
 
 export default class ShoppingCart extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      shoppingCartItems: [
-
-      ]
+      shoppingCartItems: [],
+      totalPrice: 0
     }
   }
 
   fetchShoppingCartItems() {
+    this.setState({
+      shoppingCartItems: [
+        <ShoppingCartItem
+          key={1}
+        />,
+        <ShoppingCartItem
+          key={2}
+        />,
+        <ShoppingCartItem
+          key={3}
+        />
+      ]
+    })
+  }
 
+  componentDidMount() {
+    this.fetchShoppingCartItems();
   }
 
   render() {
     return (
-      <View style={styles.mainContainer}>
+      <ScrollView style={styles.mainContainer}
+        contentContainerStyle={styles.contentContainerStyle}
+      >
         <Text style={styles.title}>
           My shopping cart
         </Text>
-        {/* shopping cart items list */}
+        {this.state.shoppingCartItems}
         <Text style={styles.summary}>
-
+          Summary: {this.state.totalPrice} zł
         </Text>
-        {/* make an order button */}
-      </View>
+        <StylizedButton
+          title={'Make an order'}
+        />
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
   mainContainer: {
-    flex: 2,
-    width: '80%',
+    flex: 1,
+    width: '100%',
+  },
+  contentContainerStyle: {
     alignItems: 'center',
   },
   title: {
     fontSize: 24,
     fontWeight: '400',
+    marginVertical: 20
   },
   summary: {
-    
+    marginVertical: 20,
+    width: '80%',
+    textAlign: 'right',
+    fontSize: 16,
+    fontWeight: '500'
   }
 })
--- components/ShoppingCart/ShoppingCartItem.js
+++ components/ShoppingCart/ShoppingCartItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
 export default class ShoppingCartItem extends React.Component {
   constructor(props) {
@@ -11,14 +11,16 @@ export default class ShoppingCartItem extends React.Component {
       <View style={styles.mainContainer}>
         <Image
           style={styles.thumbnail}
-          source={{ uri: ''}}
+          source={{ uri: 'https://static.pexels.com/photos/355296/pexels-photo-355296.jpeg'}}
         />
-        <Text style={styles.productName}>
-          Product's name
-        </Text>
-        <Text style={styles.price}>
-          39,99 zl
-        </Text>
+        <View style={styles.namePriceRow}>
+          <Text style={styles.productName}>
+            Product's name
+          </Text>
+          <Text style={styles.price}>
+            39,99 zl
+          </Text>
+        </View>
       </View>
     );
   }
@@ -26,11 +28,28 @@ export default class ShoppingCartItem extends React.Component {
 
 const styles = StyleSheet.create({
   mainContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    width: '80%',
     borderBottomWidth: 1,
-    borderBottomColor: 'grey'
+    borderBottomColor: 'grey',
+    marginBottom: 20
   },
   thumbnail: {
-    width: 32,
-    height: 32,
+    width: 80,
+    height: 80,
+  },
+  namePriceRow: {
+    flex: 1,
+    paddingLeft: 5,
+    justifyContent: 'space-between'
+  },
+  productName: {
+    fontSize: 16,
+    fontWeight: '400'
+  },
+  price: {
+    width: '100%',
+    textAlign: 'right'
   }
 })
