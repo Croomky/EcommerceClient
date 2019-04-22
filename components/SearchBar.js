1ef3145baf736d1a55fe32f1873a8a3709eaa48a
unknown <konrad96pawlak@gmail.com>
2019-04-22 13:35:49 +0200

    refactor component, add a method

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -3,18 +3,42 @@ import { StyleSheet, View, Text, TextInput } from 'react-native';
 
 import Palette from './ColorsPalette';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import NetworkConfig from './NetworkConfig';
 
-export default Header = () => (
-  <View style={styles.container}>
-    <View style={styles.searchBar}>
-      <Icon name="magnify" size={20} color={Palette.darkText} />
-      <TextInput
-        placeholder="everything You want..."
-        style={styles.textInput}
-      />
-    </View>
-  </View>
-);
+export default class Header extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  fetchQueryResult(query) {
+    var self = this;
+
+    fetch(NetworkConfig.RestApiAddress + '/product/search?q=' + query)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(queryResult) {
+        console.log(JSON.stringify(queryResult));
+        // finish
+      });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.searchBar}>
+          <Icon name="magnify" size={20} color={Palette.darkText} />
+          <TextInput
+            placeholder="everything You want..."
+            style={styles.textInput}
+          />
+        </View>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
