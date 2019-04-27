b3fa09d1a60dd970092ac90a39fbbe3aacc29eb0
unknown <konrad96pawlak@gmail.com>
2019-04-27 15:29:53 +0200

    create validatePassword and postDataForm functions

--- components/SignUp/SignUp.js
+++ components/SignUp/SignUp.js
@@ -9,6 +9,41 @@ export default class SignUp extends React.Component {
     super(props);
   }
 
+  validatePassword() {
+    if(this.state.password == this.state.confirmPassowrd) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  postFormData(state) {
+    if(!this.validatePassword()) {
+      return;
+    }
+
+    fetch('http://192.168.0.103:8000/user/register', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: state.email,
+        password: state.password
+      }),
+    }).then(function(res) {
+      return res.json();
+    }).then(function(json) {
+      if(json.answer == 'ok') {
+        console.log('SUCCESS');
+      } else if(json.answer == 'no') {
+        console.log('FAILURE');
+      }
+    }).catch(function(err) {
+      console.log(err);
+    });
+  }
+
   render() {
     return (
       <View style={styles.mainContainer}>
@@ -18,18 +53,26 @@ export default class SignUp extends React.Component {
         <TextInput
           style={styles.field}
           placeholder={"E-mail"}
+          autoCapitalize={"none"}
+          onChangeText={ (text) => this.setState({email: text}) }
         />
         <TextInput
           style={styles.field}
           placeholder={"Password"}
+          autoCapitalize={"none"}
+          secureTextEntry={true}
+          onChangeText={ (text) => this.setState({password: text}) }
         />
         <TextInput
           style={styles.field}
           placeholder={"Confirm password"}
+          autoCapitalize={"none"}
+          secureTextEntry={true}
+          onChangeText={ (text) => this.setState({confirmPassword: text}) }
         />
         <StylizedButton
           title={"Sign up"}
-          onPress={() => {}}
+          onPress={this.postFormData.bind(this.state)}
         />
       </View>
     );
