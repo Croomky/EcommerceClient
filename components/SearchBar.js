3f20d20e85c1cc1758710b1816bb2fdb7dc582d3
Konrad <konrad96pawlak@gmail.com>
2019-05-25 12:17:52 +0200

    search for product by a phrase

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -5,35 +5,39 @@ import { Redirect } from 'react-router-native';
 import Palette from './ColorsPalette';
 import History from './History';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import NetworkConfig from './NetworkConfig';
+// import NetworkConfig from './NetworkConfig';
 
 export default class SearchBar extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      searchPhrase: ''
+    };
   }
 
-  fetchQueryResult(query) {
-    var self = this;
+  // fetchQueryResult(query) {
+  //   var self = this;
 
-    fetch(NetworkConfig.RestApiAddress + '/product/search?q=' + query)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(queryResult) {
-        console.log(JSON.stringify(queryResult));
-        // finish
-      });
-  }
+  //   fetch(NetworkConfig.RestApiAddress + '/product/search?q=' + query)
+  //     .then(function(response) {
+  //       return response.json();
+  //     })
+  //     .then(function(queryResult) {
+  //       console.log(JSON.stringify(queryResult));
+  //       // finish
+  //     });
+  // }
 
-  redirectToProductList() {
-    console.log('REDIRECT!');
+  redirectToProductList(searchPhrase) {
+    // console.log('REDIRECT!');
     
-    History.push({
-      pathname: '/productList',
-      search: '?phrase=a',
-    });
+    if(searchPhrase != '') {
+      History.push({
+        pathname: '/productList',
+        search: '?phrase=' + searchPhrase,
+      });
+    }
   }
 
   render() {
@@ -44,7 +48,9 @@ export default class SearchBar extends React.Component {
           <TextInput
             placeholder="everything You want..."
             style={styles.textInput}
-            onSubmitEditing={this.redirectToProductList}
+            onSubmitEditing={this.redirectToProductList.bind(this, this.state.searchPhrase)}
+            onChangeText={ (text) => this.setState({ searchPhrase: text })}
+            value={this.state.searchPhrase}
           />
         </View>
       </View>
