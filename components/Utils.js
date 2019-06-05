9d37f5b9c08ccc70f62440aacbfa86f040428897
Konrad <konrad96pawlak@gmail.com>
2019-06-05 13:11:19 +0200

    fix prasePrice

--- components/Utils.js
+++ components/Utils.js
@@ -1,12 +1,8 @@
 export default class Utils {
   static parsePrice(price) {
     numberPrice = parseFloat(price);
-    numberPrice.toFixed(2);
+    numberPrice = numberPrice.toFixed(2);
     price = '$' + numberPrice;
-
-    // if(price.indexOf('.') == -1) {
-    //   price = price + '.00';
-    // }
 
     return price;
   }
